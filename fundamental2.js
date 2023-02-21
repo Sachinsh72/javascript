3301a81d02ef898bbc6fbafbc1bcc96f1b56dbba
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-02-21 23:16:05 +0530

    js

--- fundamental2.js
+++ fundamental2.js
@@ -14,6 +14,64 @@ console.log(a[0]);
 a[4] = 44; // updating value at the index 4
 console.log(a);
 
+//Array method 
+
+let arr = [1,2,3,4,5];
+arr.push(6,7); //add at last
+console.log(arr);
+
+arr.pop();  //remove last
+console.log(arr);
+
+arr.shift(); //remove first
+console.log(arr);
+
+arr.unshift(9); //update the value at 0th index.
+console.log(arr);
+
+// if you store shift & pop then that variable return that value example below.
+
+let array = [1,2,3,4,5,6,7,8,9]
+
+let pop = array.pop();
+console.log(pop);
+
+let shift = array.shift();
+console.log(shift);
+console.log(array);
+
+//Now for other methods
+
+let a1 = [1,2,3,4];
+let a2 = [3,4,5,6];
+let a3 = a1.concat(a2);
+console.log(a1, a2, a3);
+
+// club all elements of array
+let s = a3.join("");
+console.log(s);
+
+let p = a3.join("@");
+console.log(p);
+
+//for reverse method
+
+a3.reverse();
+console.log(a3);
+
+//for finding index of  particular number 
+console.log(a3.indexOf(4));
+
+//for fetching continues section or portion
+console.log(a3.slice(2,5));  //print -4,5,4,
+
+console.log(a3);
+
+//for adding element in middle
+a3.splice(2, 0, 11);
+
+
+
 //Array function - block of the code that performs asoecifuc task and can be reused throughout the program.
 
 // function in js are first-class citizens, meaning theycan be assigned to variables, passed as argument to other funtions, and returned firom functions.
@@ -36,7 +94,7 @@ function sqr(x){ //x--> paramenter
 
 greet();
 
-// sqr(8); // 8-->arguments
+sqr(8); // 8-->arguments
 
 let a =sqr(8);
 console.log(a);
