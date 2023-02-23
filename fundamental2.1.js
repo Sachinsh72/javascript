9e37a5a7051c50482464f90764599731d110968c
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-02-23 22:18:48 +0530

    js-function

---
+++ fundamental2.1.js
@@ -0,0 +1,50 @@
+//function without parameter
+
+function sayMessage(){
+    console.log("Hello! who are you");
+
+}
+
+sayMessage();
+sayMessage();
+sayMessage();
+sayMessage();
+
+//return
+function messages(){
+    return "Hi"
+}
+
+let message = messages();
+console.log(message);
+
+//with parameter
+
+function myFunction(x, y = 10) {
+    return x + y;
+  }
+  myFunction(5);
+
+// function sum(x, y){
+//     return x + y;
+// }
+
+// console.log(sum(4,5));
+
+// // ------
+// function add(s,v =10){
+//     return s + v;
+// }
+
+// console.log(add(4));
+
+// for default message
+// function displayMessage(){
+//     MessageToBeDisplayed = "default hiiii";
+// }
+
+// displayMessage("default message Hiiiii");
+
+// displayMessage();
+
+
