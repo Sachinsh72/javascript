5e55f8ea99bbd69527827390d2f398f8292855db
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-03-26 22:41:36 +0530

    Try and catch

--- Error Handling & Prototypes/tryCatch.js
+++ Error Handling & Prototypes/tryCatch.js
@@ -5,3 +5,39 @@ try{
 }catch{
     //block of code for addressing errors.
 }
+
+// Syntax of try - catch - finally
+
+try{
+    //block of code for testing.
+}catch{
+    //block of code for addressing errors.
+}finally{
+    // block of code to run after try-catch
+}
+
+//example 1
+
+try{
+    let x = undefined;
+    console.log(x[0]);
+}catch(err){
+    console.log("error handling in catch", err);
+}finally{
+    console.log("finally always gets executed");
+}
+
+console.log("end");
+
+// example 2
+try{
+    console.log("hello");
+    console.log(a);
+    console.log("ending try");
+}catch{
+    console.log("handled");
+}finally{
+    console.log("Finally");
+}
+
+console.log("end");
