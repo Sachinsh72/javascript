65356ce62a7e173221894fef0c5d6be763d9d81d
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-04-05 23:22:34 +0530

    closure

---
+++ Error Handling & Prototypes/closure.js
@@ -0,0 +1,20 @@
+let score = 4;
+
+function one() {
+    let score = 0;
+    console.log(score);
+}
+
+function two() {
+    let score = 3;
+    console.log(score);
+}
+
+function three() {
+    console.log(score);
+}
+
+// one();
+// two();
+// three();
+// console.log(score);
