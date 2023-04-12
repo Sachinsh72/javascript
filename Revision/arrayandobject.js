2deee2682064399a62b5eb0855129157794007f9
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-04-12 23:50:15 +0530

    array

---
+++ Revision/arrayandobject.js
@@ -0,0 +1,9 @@
+//----------Array
+
+let names = ["Sachin", "Vishal", "Suman", "Rajiv", "Ajay"];
+
+console.log(names);
+console.log("Length of array is:",names.length);
+console.log(names[4]);     //names[names.length-1] : for the last value
+names[4] = "Ajay Kumar"
+console.log(names);
