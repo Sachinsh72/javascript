bd45f2d4c3f64d3d7d2042cffeac60abdfc3748d
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-04-13 22:46:54 +0530

    array

--- Revision/arrayandobject.js
+++ Revision/arrayandobject.js
@@ -6,4 +6,13 @@ console.log(names);
 console.log("Length of array is:",names.length);
 console.log(names[4]);     //names[names.length-1] : for the last value
 names[4] = "Ajay Kumar"
-console.log(names);
+// console.log(names);
+
+//also can declare by New Keyword
+
+let naam = new Array("Sachin", "Ankur", "Aman", "jayant");
+
+//enter a new naam
+
+naam.push("Anurag");
+console.log(naam);
