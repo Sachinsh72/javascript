6b3113d10e01f7896394effc29b34e191b26fe41
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-04-16 23:43:10 +0530

    function

---
+++ Revision/function.js
@@ -0,0 +1,14 @@
+//----------Functions
+
+// function declaration
+function hello(a,b){       
+    console.log("Hello World");
+    // console.log(a+b);
+    return a+b;         // for this holder will required so the variable sum is assign for that return in below.
+}
+
+// hello(25,30);               // function call 
+let sum = hello(25,30);
+
+console.log(sum);
+console.log(hello(65,25));
