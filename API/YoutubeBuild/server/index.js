fd2666a76d6d90b44f9793a386b23a98fd249396
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-04-28 00:03:42 +0530

    server

--- API/YoutubeBuild/server/index.js
+++ API/YoutubeBuild/server/index.js
@@ -1,1 +1,12 @@
-console.log("API Creation");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/youtube', (req, res) => {
+  res.send('Hello Sachin Sharma')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
