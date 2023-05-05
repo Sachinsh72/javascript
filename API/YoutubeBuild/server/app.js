f689da46e06e1bc9e45f09f2f075820b591baa3b
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-05-05 21:20:59 +0530

    app

--- API/YoutubeBuild/server/app.js
+++ API/YoutubeBuild/server/app.js
@@ -4,14 +4,23 @@ const connectToDB = require("./configdb/db") //database connection
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+connectToDB()
+
+const userRoutes = require("./routes/userRoutes")
+
+app.use("/", userRoutes)
+
 app.get("/",(req, res)=>{
   res.send("<h1>Welcome to our server</h1>")
 
-})
+})     // route
 
 app.get("/youtube", (req, res)=>{
   res.send("<h1>Welcome to oue youtube </h1>")
-})
+})    // route
 
 app.get("/json", (req, res)=>{
   res.json({
