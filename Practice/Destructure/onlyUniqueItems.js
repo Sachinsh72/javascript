b756a8730140fc5ea19843bc6a8942e7eba37a3b
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-08 20:22:20 +0530

    Q2

--- Practice/Destructure/onlyUniqueItems.js
+++ Practice/Destructure/onlyUniqueItems.js
@@ -1,4 +1,16 @@
-/*  2. Only unique items are allowed.
+/*  Q2. Only unique items are allowed.
 You are building a program that takes an array of numbers as input and you need to remove all the duplicates
 from the array. You want to write a function that can accomplish this task efficiently and returns a new set that
 contains only the unique elements. */
+
+function uniqueElement(str){
+    return new Set(str);
+}
+
+// first array 
+const arr = [1,2,3,4,5,6,6];
+// second array 
+const arrSecond = [2,2,2,2,4];
+
+console.log(uniqueElement(arr));
+console.log(uniqueElement(arrSecond));
