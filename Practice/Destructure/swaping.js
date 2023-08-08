42e4e29bd8b5e5ea38da72f90ac2736f486ed327
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-08 20:29:12 +0530

    Q3

--- Practice/Destructure/swaping.js
+++ Practice/Destructure/swaping.js
@@ -5,3 +5,13 @@ destructuring assignment with an array. The function should take the two variabl
 them into an array, and then swap their positions within the array. Finally, the function should return an array
 with the swapped values. For example, if the function is called with the arguments x = 5 and y = 10, it should
 return an array [10, 5] with x now equal to 10 and y equal to 5. */
+
+function swapValue(x, y) {
+    [x, y] = [y, x];
+    return [x, y];
+}
+  
+let x = 1;
+let y = 2;
+
+console.log(...swapValue(x, y));
