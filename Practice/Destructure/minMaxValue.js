193938fb5d06c2af97f9a888621cc86a2432efe4
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-08 20:43:55 +0530

    Q5

--- Practice/Destructure/minMaxValue.js
+++ Practice/Destructure/minMaxValue.js
@@ -4,3 +4,19 @@ decide to write a function that can accomplish this task by taking an array of n
 spread operator with the Math methods max() and min(). The function should return an object with two
 properties, max, and min, representing the maximum and minimum values in the input array, respectively. For
 example, if the input array is [5, 2, 7, 1, 9], the function should return an object { max: 9, min: 1 }. */
+
+const array = [5, 2, 7, 1, 9];      //array
+
+let max = Math.max(...array);       //max
+let min = Math.min(...array);          //min
+
+// function return an obj 
+function minMax(arr) {
+    const obj = {
+        max : max,
+        min : min
+    }
+    return obj;
+}
+
+console.log(minMax(array));     //print return an object { max: 9, min: 1 }
