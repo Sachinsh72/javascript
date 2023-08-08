aa7d2d58efdd51e6fab3adff2fceee53acaaf3b0
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-08 20:32:25 +0530

    Q4

--- Practice/Destructure/accessRandomElement.js
+++ Practice/Destructure/accessRandomElement.js
@@ -4,3 +4,9 @@ and last elements of the array. You decide to write a function that can accompli
 assignment with an array. The function should take the array as an argument, destructure it into three variables
 representing the first, second, and last elements, and then return an array with these three values. For example,
 if the input array is [1, 2, 3, 4, 5], the function should return an array [1, 2, 5]. */
+
+const arr = [1,2,3,4,5];    // array
+
+const [first, second,,,last] = arr;     //destructuring array
+
+console.log([first, second, last]);     //print as [1, 2, 5]
