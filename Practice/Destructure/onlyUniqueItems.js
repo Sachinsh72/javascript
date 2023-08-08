3e6239773f6816839a63c6a9987d45763b392149
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-08 17:20:24 +0530

    Destructuring practice question

---
+++ Practice/Destructure/onlyUniqueItems.js
@@ -0,0 +1,4 @@
+/*  2. Only unique items are allowed.
+You are building a program that takes an array of numbers as input and you need to remove all the duplicates
+from the array. You want to write a function that can accomplish this task efficiently and returns a new set that
+contains only the unique elements. */
