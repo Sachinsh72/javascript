07cde8be336150b69990bb9b87dc18e0ad37bcac
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-08 20:49:43 +0530

    Q6

--- Practice/Destructure/nestedObject.js
+++ Practice/Destructure/nestedObject.js
@@ -1,4 +1,24 @@
 /* Q6. Nested Objects.
 Write a function that takes an object representing a person as input, and extracts the name and street
 properties from a nested object using object destructuring. The function should return an object with these two
-properties. */
+properties. A sample object person is given below */
+
+//given sample
+const person = {
+    name: "Mithun",
+    age: 21,
+    address: {
+      street: "B8, Block B, Industrial Area.",
+      city: "Sector 62, Noida",
+      state: "Uttar Pradesh",
+    },
+};
+  
+//function to extract properties name and street from the sample
+function extractProperties(obj) {
+    const { name, address: { street } } = obj;
+    return { name, street };
+}
+
+//function call
+console.log(extractProperties(person));
