640a1ed2f704c99fe1b9579dd60421372278515d
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-25 00:11:35 +0530

    Q3.

--- Practice/ErrorHandlingAndPrototype/carDescription.js
+++ Practice/ErrorHandlingAndPrototype/carDescription.js
@@ -1,3 +1,19 @@
 /* Q03. Create a class called Car with three properties: company, model, and year. The class should have a method
 called getDescription that returns a string in the format "This is a <year> <company> <model>". Instantiate an
 instance of the Car class and call the getDescription method.*/
+
+class Car{
+    company
+    model
+    year
+
+    getDescription(year,company,model){
+        this.year = year
+        this.company = company
+        this.model = model
+        return console.log(`This is ${year} ${company} ${model}`);
+    }
+}
+
+const c = new Car();
+c.getDescription(2022,"Skoda","Rapid");
