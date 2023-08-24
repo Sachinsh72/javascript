6a9ce91d42061aa5c36a673a4224115d0f256cf4
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-25 00:13:25 +0530

    Q6.

--- Practice/ErrorHandlingAndPrototype/sumCalculatorClass.js
+++ Practice/ErrorHandlingAndPrototype/sumCalculatorClass.js
@@ -1,2 +1,14 @@
 /* Q06. Create a class called Calculator with a static method called add. The add method should take two numbers as
 arguments and return their sum. Instantiate the Calculator class and call the add method. */
+
+class Calculator{
+    static add(n1,n2){
+        return n1+n2;
+    }
+}
+
+const cal = new Calculator();
+
+const result = Calculator.add(10,10) //static always refers class
+
+console.log(result);
