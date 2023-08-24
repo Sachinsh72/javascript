76d3a108aabae770f4c98a6dd941e437b0bbbd2d
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-25 00:13:05 +0530

    Q8.

--- Practice/ErrorHandlingAndPrototype/methodPrototype.js
+++ Practice/ErrorHandlingAndPrototype/methodPrototype.js
@@ -1,3 +1,15 @@
 /* Q08. Create a prototype object called Student with a property name. Add a method called printDetails to the
 prototype that logs the string "Hello, my name is {name}" to the console. Instantiate a Student object with the
 name "Mithun" and call the printDetails method.*/
+
+function Student(name){
+    this.name = name
+}
+
+Student.prototype.printDetails = function () {
+    console.log(`Hello, my name is ${this.name}`);
+}
+
+const student = new Student("Mithun");
+
+student.printDetails();
