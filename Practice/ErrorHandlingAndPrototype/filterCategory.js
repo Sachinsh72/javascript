4b080090092708801bf0b5b0feeda50253bdc4c2
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-25 00:13:52 +0530

    Q10.

--- Practice/ErrorHandlingAndPrototype/filterCategory.js
+++ Practice/ErrorHandlingAndPrototype/filterCategory.js
@@ -2,3 +2,22 @@
 category. The function must filter an eCommerce products array by a specific category. The closure filters
 products using the filter() method. Finally, it returns a new array containing only the products with the same
 category as the input.*/
+
+function filterByCategory(products){
+    return function (category){
+        return products.filter((product)=>{
+            return product.category === category;
+        })
+    }
+}
+
+const products = [
+    {name: "Shirt", category: "Clothing"},
+    {name: "Pants", category: "Clothing"},
+    {name: "Hat", category: "Accessories"},
+    {name: "Sunglasses", category: "Accessories"}
+]
+
+let clothingProducts = filterByCategory(products)("Clothing");
+
+console.log(clothingProducts);
