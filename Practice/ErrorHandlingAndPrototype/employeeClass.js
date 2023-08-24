5aae60db9ce2534275d3589820e6c32f741efbc9
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-25 00:11:50 +0530

    Q4.

--- Practice/ErrorHandlingAndPrototype/employeeClass.js
+++ Practice/ErrorHandlingAndPrototype/employeeClass.js
@@ -1,3 +1,22 @@
 /* Q04. Create a class called Employee with three properties: name, position, and salary. The class should have a
 method called getSalary that returns the employee's salary. Instantiate an instance of the Employee class and
 call the getSalary method.*/
+
+class Employee{
+    name
+    position
+    salary
+
+    constructor(name,position,salary){
+        this.name = name
+        this.position = position
+        this.salary = salary
+    }
+    getSalary(){
+        return this.salary
+    }
+}
+
+const e = new Employee("Sachin","Software Developer",90000);
+
+console.log(e.getSalary());
