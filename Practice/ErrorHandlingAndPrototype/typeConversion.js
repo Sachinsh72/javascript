4b8f641c382165f2540791110a7ba7225408b4fa
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-24 19:44:25 +0530

    Q1.

--- Practice/ErrorHandlingAndPrototype/typeConversion.js
+++ Practice/ErrorHandlingAndPrototype/typeConversion.js
@@ -1,3 +1,17 @@
 /* Q01. Write a function called convertToNumber that takes a string as an argument and returns the equivalent
 number. If the string cannot be converted to a number, the function should return the string "Invalid number".
 Use error handling in javascript to achieve this output.*/
+function convertToNumber(str){
+    try {
+        const number = Number(str);
+        if(Number.isNaN(number)){
+            throw new Error("Invalid number");
+        }
+        return number;
+    
+    } catch (error) {
+        return error.message;
+    }
+}
+console.log(convertToNumber("123"));
+console.log(convertToNumber("abc"));
