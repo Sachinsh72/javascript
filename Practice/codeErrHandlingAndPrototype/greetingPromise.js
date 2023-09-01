8097c903a72782c52b5850052d5440cd7f41d28d
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-09-01 13:14:41 +0530

    Q5.

---
+++ Practice/codeErrHandlingAndPrototype/greetingPromise.js
@@ -0,0 +1,14 @@
+/* Q5. You need to write a function that takes a name as input and returns a promise that resolves with a greeting
+message. The function should greet the person using their name, with a message in the format "Hello, {name}!".
+For example, if the input to the function is "Mithun", the promise should resolve with the string "Hello, Mithun!". */
+
+const name = "Sachin";
+
+function greet(name){
+    return new Promise((resolve)=>{
+        const greet = `Hello ${name}`;
+        resolve(greet);
+    })
+}
+
+let greetPromise = greet(name).then((message)=> console.log(message));
